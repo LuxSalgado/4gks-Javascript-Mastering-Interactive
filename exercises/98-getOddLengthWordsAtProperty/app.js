c82cdd081f0defdb856c3f37ad3c59de41a5d277
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-07-30 23:49:33 +0000

    Ejercicios resueltos hasta el número 99 - Todos los ejercicios completados

--- exercises/98-getOddLengthWordsAtProperty/app.js
+++ exercises/98-getOddLengthWordsAtProperty/app.js
@@ -1,1 +1,10 @@
 // Write your function here
+function getOddLengthWordsAtProperty (obj,llave){
+    let aux =  obj[llave].filter(palabra => palabra.length % 2 != 0);
+    return aux;
+}
+var obj = {
+    key: ['It', 'has', 'some', 'words']
+  };
+  var output = getOddLengthWordsAtProperty(obj, 'key');
+  console.log(output); // --> ['has', 'words']
--- exercises/99-computeAverageOfNumbers/app.js
+++ exercises/99-computeAverageOfNumbers/app.js
@@ -1,1 +1,11 @@
 // Write your function here
+function computeAverageOfNumbers (array){
+    let sum = 0;
+    for (let i=0; i<array.length; i++){
+        sum = sum + array[i];
+    }
+    return sum/array.length;
+}
+var input = [1,2,3,4,5];
+var output = computeAverageOfNumbers(input);
+console.log(output); // --> 3
