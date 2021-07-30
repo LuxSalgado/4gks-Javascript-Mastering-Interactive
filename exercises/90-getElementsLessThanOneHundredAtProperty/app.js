0bfe9c4200af3ebe8f5be8880c81dd0706fc645a
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-07-30 22:55:55 +0000

    Ejercicios resueltos hasta el número 90

--- exercises/90-getElementsLessThanOneHundredAtProperty/app.js
+++ exercises/90-getElementsLessThanOneHundredAtProperty/app.js
@@ -1,1 +1,16 @@
 // Write your function here
+function getElementsLessThan100AtProperty (objeto,llave) {
+  let objNuevo = [];
+    console.log(objeto[llave].length)
+    for (let i=0; i<objeto[llave].length; i++){
+        if (objeto[llave][i] < 100){
+            objNuevo.push(objeto[llave][i]);
+        }
+    }
+    return objNuevo;
+}
+var obj = {
+    key: [1000, 20, 50, 500]
+  };
+  var output = getElementsLessThan100AtProperty(obj, 'key');
+  console.log(output); // --> [20, 50]
